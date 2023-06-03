cca31038e2a66fea5dbcca76c04bba7cf7fd4dc5
Arthur <106569079+arthurstrelow@users.noreply.github.com>
2023-06-03 11:28:27 -0300

    nova att do class.js
    Otimização com o uso de variaveis virando uma única função.

--- class.js
+++ class.js
@@ -23,36 +23,37 @@ class validator_cpf_cnpj{
         }
     }
 
-    validandoCPF(cpf){
-        let valores = {
-            peso1: 10,
-            peso2: 11,
-            primeirasoma: 0,
-            segundasoma: 0,
-        }
-        for (let numero in cpf.slice(0, 9)){
-            valores.primeirasoma += cpf[numero] * valores.peso1
-            valores.peso1--
-        }
-        if(valores.primeirasoma === 10) valores.primeirasoma = 10
-        if(!((valores.primeirasoma * 10) % 11 === parseInt(cpf.slice(9,10)))) return false
-        for (let numero in cpf.slice(0,10)){
-            valores.segundasoma += cpf[numero] * valores.peso2
-            valores.peso2--
-        }
-        if(valores.segundasoma === 10) valores.segundasoma = 0
-        return (valores.segundasoma * 10) % 11 === parseInt(cpf.slice(10,11))
-    }
-
-    validandoCNPJ(cnpj){
-        let calculos = {
-            peso1: 5,
-            peso2: 9,
+    objetoDeCalculos(){
+        return {
+            peso1: 0,
+            peso2: 0,
             primeirosoma: 0,
             segundosoma: 0,
             digitoum: 0,
             digitodois: 0
         }
+    }
+
+    validandoCPF(cpf){
+        let valores = this.objetoDeCalculos()
+        valores.peso1 = 10;valores.peso2 = 11
+        for (let numero in cpf.slice(0, 9)){
+            valores.primeirosoma += cpf[numero] * valores.peso1
+            valores.peso1--
+        }
+        if(valores.primeirosoma === 10) valores.primeirosoma = 10
+        if(!((valores.primeirosoma * 10) % 11 === parseInt(cpf.slice(9,10)))) return false
+        for (let numero in cpf.slice(0,10)){
+            valores.segundosoma += cpf[numero] * valores.peso2
+            valores.peso2--
+        }
+        if(valores.segundosoma === 10) valores.segundosoma = 0
+        return (valores.segundosoma * 10) % 11 === parseInt(cpf.slice(10,11))
+    }
+
+    validandoCNPJ(cnpj){
+        let calculos = this.objetoDeCalculos()
+        calculos.peso1 = 5;calculos.peso2 = 9;
         for(let numero in cnpj.slice(0,12)){
             if(calculos.peso1 >= 2){
                 calculos.primeirosoma += cnpj[numero] * calculos.peso1
